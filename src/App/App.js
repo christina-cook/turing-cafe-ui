4374191332da208d63739aff8b2ca0aae44805be
Christina Cook <cmc.cook7@gmail.com>
2021-02-17 09:45:38 -0700

    Fetch all reservations and set App state

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Reservations from '../Reservations/Reservations';
 import Card from '../Card/Card';
 import Form from '../Form/Form';
+import getAllReservations from '../API/apiCalls';
 
 class App extends Component {
   constructor() {
@@ -10,6 +11,14 @@ class App extends Component {
     this.state = {
       reservations: []
     }
+  }
+
+  componentDidMount() {
+    getAllReservations()
+      .then(data => this.setState({
+        reservations: data
+      }))
+      .catch(error => console.log(error))
   }
 
   render() {
