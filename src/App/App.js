49714438114fe2a0711aa48c90a944fd90cc3444
Christina Cook <cmc.cook7@gmail.com>
2021-02-17 09:23:31 -0700

    Create Reservations component and connect to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Reservations from '../Reservations/Reservations'
 
 class App extends Component {
   constructor() {
@@ -8,7 +9,7 @@ class App extends Component {
       reservations: []
     }
   }
-  
+
   render() {
     return (
       <div className="App">
@@ -17,7 +18,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-
+          <Reservations />
         </div>
       </div>
     )
---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Reservations.css'
+
+const Reservations = () => {
+  return (
+    <h1>Reservations go here</h1>
+  )
+}
+
+export default Reservations;
