e7b2edf5b01fc98b972e29a6177b0e7949315efd
Christina Cook <cmc.cook7@gmail.com>
2021-02-17 11:10:51 -0700

    Write initial it statements for page display

---
+++ cypress/integration/reservations_spec.js
@@ -0,0 +1,19 @@
+describe('Turing Cafe Reservations', () => {
+  const baseURL = 'http://localhost:3000/'
+
+  beforeEach(() => {
+    cy.visit(baseURL)
+  })
+
+  it('Should display the app\'s name at the top of the page', () => {
+
+  })
+
+  it('Should display a form for a user to make a reservation', () => {
+
+  })
+
+  it('Should display all existing reservations below the form', () => {
+
+  })
+})
